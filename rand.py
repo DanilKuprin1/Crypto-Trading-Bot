0a84da089d64b4b9f4f4d8529fdbae2c6be364f0
DanilKuprin1 <danilkuprinca@gmail.com>
2023-05-11 15:00:55 -0400

    unnecessary files were deleted

--- rand.py
+++
@@ -1,3 +0,0 @@
-import random
-for _ in range(0,6):
-    print(f"{random.randint(0,9)}")
--- try.py
+++
@@ -1,7 +0,0 @@
-from os import getpid
-from sys import api_version
-import time
-import Config
-from binance import Client
-
-
