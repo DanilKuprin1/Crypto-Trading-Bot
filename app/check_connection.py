0c3cb791dad2d0d6854bb2a1dfe02990415a5762
DanilKuprin1 <danilkuprinca@gmail.com>
2025-03-19 18:57:39 -0400

    Refactoring check_connection

--- app/check_connection.py
+++ app/check_connection.py
@@ -1,17 +1,15 @@
 from time import sleep, time
 
 from binance.client import Client
-
 from config import Config
 
-totalTime = 0
-numOfTimes = 0
-for n in range(1, 180):
-    time1 = time()
+durations = []
+
+for _ in range(179):
+    start_time = time()
     client = Client(api_key=Config.API_KEY, api_secret=Config.API_SECRET)
-    time2 = time()
-    totalTime += time2 - time1
-    numOfTimes += 1
+    durations.append(time() - start_time)
     sleep(1)
 
-print(f"Average time = {totalTime / numOfTimes}")
+average_time = sum(durations) / len(durations)
+print(f"Average time = {average_time:.4f} seconds")
